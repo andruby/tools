385b819d727e1afc3ad89f80c01fed3fec4f98a3
Andrew Fecheyr <andrew@bedesign.be>
2015-12-30 00:13:35 +0100

    Use percentages and colorcode the positive and negative returns

--- histogram/histogram.r
+++ histogram/histogram.r
@@ -3,11 +3,11 @@
 # Load libraries
 library(jsonlite)
 
-data_name <- "ivv"
+data_name <- "spy"
 
 # Load data
 data <- read.csv(paste("~/Downloads/", data_name, ".csv", sep=""))
-x <- na.omit(data$Diff.percent)
+x <- na.omit(data$Diff.percent*100)
 summary(x)
 
 info <- c()
@@ -24,14 +24,19 @@ write(toJSON(info, pretty=TRUE), file = paste(data_name, ".json", sep=""))
 
 # Build histogram
 pdf(paste(data_name, ".pdf", sep=""), bg = "white")
-par(mar = c(4,4,0.5,1))
-h <- hist(x, col = "lightblue", xlab = "% difference over 364 days", main= "", breaks = seq(min(x), max(x), l=30))
+par(mar = c(5,5,2,2))
+group_width <- 5
+low_group <- floor(min(x)*(1/group_width))/(1/group_width)
+high_group <- ceiling(max(x)*(1/group_width))/(1/group_width)
+negative_groups <- abs(min(low_group/group_width, 0))
+positive_groups <- max(high_group/group_width, 0)
+h <- hist(x, col = c(rep("#e34a33", negative_groups), rep("#2ca25f", positive_groups)), main = data_name, xlab = "% difference over 364 days", breaks = seq(low_group, high_group, by=group_width))
 
 # With Normal line
 xfit <- seq(min(x), max(x), length = 80)
 yfit <- dnorm(xfit, mean = mean(x), sd = sd(x))
 yfit <- yfit * diff(h$mids[1:2]) * length(x)
-lines(xfit, yfit, col = "black", lwd = 2)
+lines(xfit, yfit, col = "black", lwd = 1)
 
 dev.off()
 
